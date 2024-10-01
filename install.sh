2a93a47111b7aa746439a00ef85eb62383616eb4
ミドリ <midori01@users.noreply.github.com>
2024-10-01 23:48:43 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ git clone https://github.com/TeamPGM/PagerMaid-Pyro.git /var/lib/pagermaid
 pip3 install -r /var/lib/pagermaid/requirements.txt --break-system-packages
 mkdir -p /var/lib/pagermaid/data
 wget -P /var/lib/pagermaid/data https://raw.githubusercontent.com/midori01/pagermaid/refs/heads/main/data/config.yml
+
 cat <<'TEXT' > /etc/systemd/system/pagermaid.service
 [Unit]
 Description=PagerMaid-Pyro Telegram Utility Daemon
@@ -19,5 +20,17 @@ WorkingDirectory=/var/lib/pagermaid
 ExecStart=/usr/bin/python3 -m pagermaid
 Restart=always
 TEXT
+
 systemctl daemon-reload
 systemctl enable pagermaid
+
+read -p "Custom command prefix? (Default: N) [Y/N]: " modify_prefix
+[[ "$modify_prefix" =~ ^[Yy]$ ]] || { echo "No change."; exit; }
+read -p "New command prefix: " new_prefix
+if [[ -z "$new_prefix" ]]; then
+    echo "Cannot be empty."
+    exit 1
+fi
+sed -i.bak "s/,|，/${new_prefix}/g" /var/lib/pagermaid/pagermaid/listener.py
+
+cd /var/lib/pagermaid
