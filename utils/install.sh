52e6493ca733a3570e60306e55ddc37db17b8569
ミドリ <midori01@users.noreply.github.com>
2024-10-02 23:46:41 +0900

    Update install.sh

--- utils/install.sh
+++ utils/install.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 apt update && apt install git wget curl python3 python3-pip neofetch -y
-git clone https://github.com/TeamPGM/PagerMaid-Pyro.git /var/lib/pagermaid
+git clone https://github.com/midori01/pagermaid.git /var/lib/pagermaid
 pip3 install -r /var/lib/pagermaid/requirements.txt --break-system-packages
-mkdir -p /var/lib/pagermaid/data
-wget -P /var/lib/pagermaid/data https://raw.githubusercontent.com/midori01/pagermaid/main/data/config.yml
 cat <<'TEXT' > /etc/systemd/system/pagermaid.service
 [Unit]
 Description=PagerMaid-Pyro Telegram Utility Daemon
