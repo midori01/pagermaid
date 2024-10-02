a785b7c0416488dc886bb4dd1599530a44261c14
ミドリ <midori01@users.noreply.github.com>
2024-10-02 12:12:10 +0900

    Update install.sh

--- utils/install.sh
+++ utils/install.sh
@@ -5,7 +5,6 @@ git clone https://github.com/TeamPGM/PagerMaid-Pyro.git /var/lib/pagermaid
 pip3 install -r /var/lib/pagermaid/requirements.txt --break-system-packages
 mkdir -p /var/lib/pagermaid/data
 wget -P /var/lib/pagermaid/data https://raw.githubusercontent.com/midori01/pagermaid/main/data/config.yml
-
 cat <<'TEXT' > /etc/systemd/system/pagermaid.service
 [Unit]
 Description=PagerMaid-Pyro Telegram Utility Daemon
@@ -20,10 +19,12 @@ WorkingDirectory=/var/lib/pagermaid
 ExecStart=/usr/bin/python3 -m pagermaid
 Restart=always
 TEXT
-
 systemctl daemon-reload
 systemctl enable pagermaid
-
+read -p "API ID: " api_id
+read -p "API Hash: " api_hash
+sed -i "s/^api_id:.*/api_id: \"$api_id\"/" /var/lib/pagermaid/data/config.yml
+sed -i "s/^api_hash:.*/api_hash: \"$api_hash\"/" /var/lib/pagermaid/data/config.yml
 read -p "Custom command prefix? (Default: N) [Y/N]: " modify_prefix
 [[ "$modify_prefix" =~ ^[Yy]$ ]] || { echo "No change."; exit; }
 read -p "New command prefix: " new_prefix
@@ -32,5 +33,4 @@ if [[ -z "$new_prefix" ]]; then
     exit 1
 fi
 sed -i.bak "s/,|，/${new_prefix}/g" /var/lib/pagermaid/pagermaid/listener.py
-
 cd /var/lib/pagermaid && python3 -m pagermaid
