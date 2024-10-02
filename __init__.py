6fee2f3a421c87e35125e78b7264a79313dac172
ミドリ <midori01@users.noreply.github.com>
2024-10-02 23:17:58 +0900

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,31 @@
+"""
+pyromod - A monkeypatched add-on for Pyrogram
+Copyright (C) 2020 Cezar H. <https://github.com/usernein>
+
+This file is part of pyromod.
+
+pyromod is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+pyromod is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with pyromod.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+__version__ = 2
+
+
+def require_mod_version(version: int = __version__):
+    if version > __version__:
+        raise ImportError("This plugin requires higher pagermaid-pyro version")
+
+    def decorator(function):
+        return function
+
+    return decorator
