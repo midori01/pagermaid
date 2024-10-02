fcdd0ed3542f7722d0895e80d8c17e9b34dde40e
ミドリ <midori01@users.noreply.github.com>
2024-10-02 23:37:55 +0900

    Add files via upload

---
+++ pagermaid/service/__init__.py
@@ -0,0 +1,24 @@
+from pagermaid import bot
+from pagermaid import logs
+from pagermaid.single_utils import sqlite
+from pagermaid.scheduler import scheduler
+from pagermaid.utils import client
+
+__all__ = [
+    "bot",
+    "logs",
+    "sqlite",
+    "scheduler",
+    "client",
+]
+
+
+def get(name: str):
+    data = {
+        "Client": bot,
+        "Logger": logs,
+        "SqliteDict": sqlite,
+        "AsyncIOScheduler": scheduler,
+        "AsyncClient": client,
+    }
+    return data.get(name)
