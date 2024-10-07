03d4abd7869d315e53d02e4023ca5c4731a54127
ミドリ <midori01@users.noreply.github.com>
2024-10-08 00:06:00 +0900

    Update speedtest.py

--- plugins/speedtest.py
+++ plugins/speedtest.py
@@ -110,7 +110,7 @@ async def run_speedtest(request: AsyncClient, message: Message):
         f"`  ISP`` ``{result['isp']} {await get_as_info(request, result['interface']['externalIp'])}`\n"
         f"` Node`` ``{result['server']['id']}` - `{result['server']['name']}` - `{result['server']['location']}`\n"
         f"`Speed`` `↓`{await unit_convert(result['download']['bandwidth'])}`` `↑`{await unit_convert(result['upload']['bandwidth'])}`\n"
-        f"` Ping`` `⇔`{result['ping']['latency']}ms`` `~`{result['ping']['jitter']}ms`\n"
+        f"` Ping`` `⇔`{result['ping']['latency']}ms`` `±`{result['ping']['jitter']}ms`\n"
         f"` Time`` ``{result['timestamp'].replace('T', ' ').split('.')[0].replace('Z', '')}`"
     )
 
