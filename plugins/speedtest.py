25f57509882c2528a3f1b023e2f25da43a16c0d0
ミドリ <midori01@users.noreply.github.com>
2024-10-07 22:54:03 +0900

    Update speedtest.py

--- plugins/speedtest.py
+++ plugins/speedtest.py
@@ -106,12 +106,11 @@ async def run_speedtest(request: AsyncClient, message: Message):
         return lang('speedtest_ConnectFailure'), None
 
     des = (
-        f"**Speedtest by Ookla**\n"
-        f"回線業者 `{result['isp']} {await get_as_info(request, result['interface']['externalIp'])}`\n"
-        f"サーバー `{result['server']['id']}` - `{result['server']['name']}` - `{result['server']['location']}`\n"
-        f"通信速度 ↓`{await unit_convert(result['download']['bandwidth'])}` | ↑`{await unit_convert(result['upload']['bandwidth'])}`\n"
-        f"遅延時間 ⇔`{result['ping']['latency']}ms` / ~`{result['ping']['jitter']}ms`\n"
-        f"測定時間 `{result['timestamp'].replace('T', ' ').split('.')[0].replace('Z', '')}`"
+        f"回線業者` ``{result['isp']} {await get_as_info(request, result['interface']['externalIp'])}`\n"
+        f"サーバー` ``{result['server']['id']}` - `{result['server']['name']}` - `{result['server']['location']}`\n"
+        f"通信速度` `↓`{await unit_convert(result['download']['bandwidth'])}` / ↑`{await unit_convert(result['upload']['bandwidth'])}`\n"
+        f"遅延時間` `⇔`{result['ping']['latency']}ms` / ~`{result['ping']['jitter']}ms`\n"
+        f"測定時間` ``{result['timestamp'].replace('T', ' ').split('.')[0].replace('Z', '')}`"
     )
 
     if result["result"]["url"]:
