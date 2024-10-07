66cff9eb729adb50860dfff992564e6923581399
ミドリ <midori01@users.noreply.github.com>
2024-10-07 21:39:46 +0900

    Update speedtest.py

--- plugins/speedtest.py
+++ plugins/speedtest.py
@@ -106,6 +106,7 @@ async def run_speedtest(request: AsyncClient, message: Message):
         return lang('speedtest_ConnectFailure'), None
 
     des = (
+        f"**Speedtest by Ookla**\n"
         f"`    ISP`: `{result['isp']} {await get_as_info(request, result['interface']['externalIp'])}`\n"
         f"` Server`: `{result['server']['id']}` - `{result['server']['name']}` - `{result['server']['location']}`\n"
         f"`  Speed`: ↓`{await unit_convert(result['download']['bandwidth'])}` | ↑`{await unit_convert(result['upload']['bandwidth'])}`\n"
