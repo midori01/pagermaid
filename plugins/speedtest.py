50e3482d5fd2f0524a2ae74af966b8f6c764ca60
ミドリ <midori01@users.noreply.github.com>
2024-10-07 23:29:08 +0900

    Update speedtest.py

--- plugins/speedtest.py
+++ plugins/speedtest.py
@@ -106,11 +106,12 @@ async def run_speedtest(request: AsyncClient, message: Message):
         return lang('speedtest_ConnectFailure'), None
 
     des = (
-        f"回線業者` ``{result['isp']} {await get_as_info(request, result['interface']['externalIp'])}`\n"
-        f"サーバー` ``{result['server']['id']}` - `{result['server']['name']}` - `{result['server']['location']}`\n"
-        f"通信速度` `↓`{await unit_convert(result['download']['bandwidth'])}` / ↑`{await unit_convert(result['upload']['bandwidth'])}`\n"
-        f"遅延時間` `⇔`{result['ping']['latency']}ms` / ~`{result['ping']['jitter']}ms`\n"
-        f"測定時間` ``{result['timestamp'].replace('T', ' ').split('.')[0].replace('Z', '')}`"
+        f"**Speedtest**\n"
+        f"•`  ISP`: `{result['isp']} {await get_as_info(request, result['interface']['externalIp'])}`\n"
+        f"•` Node`: `{result['server']['id']}` - `{result['server']['name']}` - `{result['server']['location']}`\n"
+        f"•`Speed`: ↓`{await unit_convert(result['download']['bandwidth'])}` / ↑`{await unit_convert(result['upload']['bandwidth'])}`\n"
+        f"•` Ping`: ⇔`{result['ping']['latency']}ms` / ~`{result['ping']['jitter']}ms`\n"
+        f"•` Time`: `{result['timestamp'].replace('T', ' ').split('.')[0].replace('Z', '')}`"
     )
 
     if result["result"]["url"]:
