6f78e17361f10fc770fd4209577b62c28a210705
ミドリ <midori01@users.noreply.github.com>
2024-10-10 22:08:52 +0900

    Update speedtest.py

--- plugins/speedtest.py
+++ plugins/speedtest.py
@@ -128,7 +128,7 @@ async def run_speedtest(request: AsyncClient, message: Message):
     as_info, cc_code, cc_flag = await get_ip_api(request, result['interface']['externalIp'])
     
     des = (
-        f"> **⚡️SPEEDTEST by OOKLA [@{cc_flag}](https://t.me/c/{str(message.chat.id).lstrip('-100')}/{message.id})**\n"
+        f"> **⚡️SPEEDTEST by OOKLA [@{cc_flag}](https://t.me/c/{str(message.chat.id)[4:] if message.chat.id < 0 else message.chat.id}/{message.id})**\n"
         f"`Info``  ``{result['isp']}`` `[{as_info}](https://bgp.he.net/{as_info})\n"
         f"`Node``  ``{result['server']['id']}` - `{result['server']['name']}` - `{result['server']['location']}`\n"
         f"`Ping``  `⇔`{result['ping']['latency']}ms`` `±`{result['ping']['jitter']}ms`\n"
